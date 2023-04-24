aaba4214e59810bdd880748bfdb56a86280dd21e
clchen-arcadia <clchen.arcadia@gmail.com>
2023-04-24 14:32:11 -0400

    Add some display functionality and fix some style

--- src/Snowman.js
+++ src/Snowman.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 
 import "./Snowman.css";
 import img0 from "./0.png";
@@ -8,6 +8,7 @@ import img3 from "./3.png";
 import img4 from "./4.png";
 import img5 from "./5.png";
 import img6 from "./6.png";
+import {randomWord, ENGLISH_WORDS} from "./words";
 
 
 /** Snowman game: plays hangman-style game with a melting snowman.
@@ -23,24 +24,28 @@ import img6 from "./6.png";
  * - answer: selected secret word*
  */
 
-function Snowman({
+function Snowman(
+    {
       images=[img0, img1, img2, img3, img4, img5, img6],
-      words=["apple"],
       maxWrong=6,
+      words=ENGLISH_WORDS,
     }) {
-  /** by default, allow 6 guesses and use provided gallows images. */
-
   const [nWrong, setNWrong] = useState(0);
   const [guessedLetters, setGuessedLetters] = useState(() => new Set());
-  const [answer, setAnswer] = useState((words)[0]);
+  const [answer, setAnswer] = useState(() => randomWord(words));
+
+  /** reset: restart game from beginning. */
+  function reset() {
+    setNWrong(0);
+    setGuessedLetters(new Set());
+    setAnswer(randomWord(words));
+  }
 
   /** guessedWord: show current-state of word:
    if guessed letters are {a,p,e}, show "app_e" for "apple"
    */
   function guessedWord() {
-    return answer
-        .split("")
-        .map(ltr => (guessedLetters.has(ltr) ? ltr : "_"));
+    return answer.split("").map(ltr => (guessedLetters.has(ltr) ? ltr : "_"));
   }
 
   /** handleGuess: handle a guessed letter:
@@ -49,8 +54,7 @@ function Snowman({
    */
   function handleGuess(evt) {
     let ltr = evt.target.value;
-
-    setGuessedLetters(g => {
+    setGuessedLetters(function (g) {
       const newGuessed = new Set(g);
       newGuessed.add(ltr);
       return newGuessed;
@@ -73,11 +77,30 @@ function Snowman({
     ));
   }
 
+  /** generateMsgOrButtons: return buttons or lose/win msg */
+  function generateMsgOrButtons() {
+    let won = !(guessedWord().includes("_"));
+    if (won) {
+      return "You won!";
+    } else if (nWrong < maxWrong) {
+      return generateButtons();
+    } else {
+      return `You lose: ${answer}`;
+    }
+  }
+
   return (
       <div className="Snowman">
-        <img src={(images)[nWrong]} alt={nWrong} />
+        <img
+            alt={`${Math.max(maxWrong - nWrong, 0)} guesses left`}
+            src={(images)[nWrong]}
+        />
+        <p>Wrong: {nWrong}</p>
         <p className="Snowman-word">{guessedWord()}</p>
-        <p>{generateButtons()}</p>
+        <p>{generateMsgOrButtons()}</p>
+        <button className="Snowman-restart" onClick={reset}>
+          Restart
+        </button>
       </div>
   );
 }
